1c4a79f8b8f122561c69b17fce1b04e33ca1f5d3
hafizmuzaffar <57796514+hafizmuzaffar@users.noreply.github.com>
2020-08-07 12:19:00 +0700

    Add files via upload

---
+++ database/migrations/2020_08_07_023904_create_like_dislike_jawaban_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikeDislikeJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('like_dislike_jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('jawaban_id');
+            $table->foreign('jawaban_id')->references('id')->on('jawabans');
+
+            $table->unsignedBigInteger('profile_id');
+            $table->foreign('profile_id')->references('id')->on('profiles');
+            
+            $table->integer('poin')->nullable();
+            $table->primary(['jawaban_id', 'profile_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('like_dislike_jawaban');
+    }
+}
